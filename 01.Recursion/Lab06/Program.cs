9683635d0a7c995ce4f31e3d23eaef3ad3cb2622
Tihomir Dimov <tihomirdimov@tihomirdimov.com>
2017-07-10 09:59:28 +0300

    problems06-07 added

--- 01.Recursion/Lab06/Program.cs
+++ 01.Recursion/Lab06/Program.cs
@@ -1,15 +1,85 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lab06
 {
     class Program
     {
-        static void Main(string[] args)
+        static bool[,] board = new bool[8, 8];
+        static HashSet<int> attckedRows = new HashSet<int>();
+        static HashSet<int> attckedCols = new HashSet<int>();
+        static HashSet<int> attckedLeftDiagonals = new HashSet<int>();
+        static HashSet<int> attckedRightDiagonals = new HashSet<int>();
+
+        static void Main()
         {
+            PlaceQueen(0);
+        }
+
+        public static void PlaceQueen(int row)
+        {
+            if (row == 8)
+            {
+                Print();
+            }
+            else
+            {
+                for (int col = 0; col < 8; col++)
+                {
+                    if (PlaceQueen(row, col))
+                    {
+                        Mark(row, col);
+                        PlaceQueen(row + 1);
+                        Unmark(row, col);
+                    }
+                }
+            }
+        }
+
+        public static bool PlaceQueen(int row, int col)
+        {
+            return !attckedRows.Contains(row)
+                   && !attckedCols.Contains(col)
+                   && !attckedLeftDiagonals.Contains(row + col)
+                   && !attckedRightDiagonals.Contains(row - col);
+        }
+
+        public static void Mark(int row, int col)
+        {
+            board[row, col] = true;
+            attckedRows.Add(row);
+            attckedCols.Add(col);
+            attckedLeftDiagonals.Add(row + col);
+            attckedRightDiagonals.Add(row - col);
+        }
+
+        public static void Unmark(int row, int col)
+        {
+            board[row, col] = false;
+            attckedRows.Remove(row);
+            attckedCols.Remove(col);
+            attckedLeftDiagonals.Remove(row + col);
+            attckedRightDiagonals.Remove(row - col);
+        }
+
+        public static void Print()
+        {
+            for (int row = 0; row < 8; row++)
+            {
+                for (int col = 0; col < 8; col++)
+                {
+                    if (board[row, col])
+                    {
+                        Console.Write("* ");
+                    }
+                    else
+                    {
+                        Console.Write("- ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
         }
     }
 }
---
+++ 01.Recursion/Lab07/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab07
+{
+    class Program
+    {
+        static List<char> path = new List<char>();
+        static char[,] lab =
+        {
+            {'-', '-', '-', '*', '-', '-', '-'},
+            {'*', '*', '-', '*', '-', '*', '-'},
+            {'-', '-', '-', '-', '-', '-', '-'},
+            {'-', '*', '*', '*', '*', '*', '-'},
+            {'-', '-', '-', '-', '-', '-', 'e'},
+        };
+
+        static void Main(string[] args)
+        {
+            FindPaths(0, 0, 'S');
+        }
+
+        private static void FindPaths(int row, int col, char direction)
+        {
+            if (!IsValidCell(row, col))
+            {
+                return;
+            }
+            path.Add(direction);
+            if (lab[row, col] == 'e')
+            {
+                PrintPath();
+            }
+            else if (lab[row, col] != 'v')
+            {
+                lab[row, col] = 'v';
+                FindPaths(row, col + 1, 'R');
+                FindPaths(row + 1, col, 'D');
+                FindPaths(row, col - 1, 'L');
+                FindPaths(row - 1, col, 'U');
+                lab[row, col] = '-';
+            }
+            path.RemoveAt(path.Count-1);
+        }
+
+        private static bool IsValidCell(int row, int col)
+        {
+            return row >= 0 &&
+                   row < lab.GetLength(0) &&
+                   col >= 0 &&
+                   col < lab.GetLength(1) &&
+                   lab[row, col] != '*';
+        }
+
+        private static void PrintPath()
+        {
+            Console.WriteLine(string.Join("", path));
+        }
+    }
+}
